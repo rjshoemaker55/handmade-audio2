97bd1744642164961302c90039baaff2cfe484ab
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-01-11 07:59:24 -0500

    Updates

---
+++ precache-manifest.1f834262f147f4052cd850d6e564afbc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7615713f25d1f480b78b65ba5eced48b",
+    "url": "/handmade-audio2/index.html"
+  },
+  {
+    "revision": "580c8dbcdb9a58e09890",
+    "url": "/handmade-audio2/static/css/main.8d76cf47.chunk.css"
+  },
+  {
+    "revision": "694468a1e8f3aa69ef91",
+    "url": "/handmade-audio2/static/js/2.4d3a489e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/handmade-audio2/static/js/2.4d3a489e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "580c8dbcdb9a58e09890",
+    "url": "/handmade-audio2/static/js/main.6eac52f4.chunk.js"
+  },
+  {
+    "revision": "bdfd4cb03d0c1c416e40",
+    "url": "/handmade-audio2/static/js/runtime-main.20aeefbe.js"
+  },
+  {
+    "revision": "acadbe795764b0529a6da5286f204788",
+    "url": "/handmade-audio2/static/media/Soundwave_logo-black.acadbe79.png"
+  },
+  {
+    "revision": "80de01a15ba85b2460ce3861401aeb6d",
+    "url": "/handmade-audio2/static/media/Soundwave_logo_white.80de01a1.png"
+  },
+  {
+    "revision": "5c7b1b15181598afc128f5097f7834c2",
+    "url": "/handmade-audio2/static/media/contact-background.5c7b1b15.jpg"
+  },
+  {
+    "revision": "4ff70a025f0e58253c9354c45abe4fab",
+    "url": "/handmade-audio2/static/media/credo_audio.4ff70a02.jpeg"
+  },
+  {
+    "revision": "e65988cbed1bad868a75ccbf8641b273",
+    "url": "/handmade-audio2/static/media/credo_audio.e65988cb.png"
+  },
+  {
+    "revision": "1f59fae8c18b3e3b74a3b825fdc7f1e1",
+    "url": "/handmade-audio2/static/media/focal_audio.1f59fae8.jpg"
+  },
+  {
+    "revision": "9efbe542e9ec874aed0ee1e17e95e920",
+    "url": "/handmade-audio2/static/media/focal_logo.9efbe542.png"
+  },
+  {
+    "revision": "c83b0154dbf6dafcf9a60de2296000f5",
+    "url": "/handmade-audio2/static/media/iso_acoustics.c83b0154.png"
+  },
+  {
+    "revision": "7c13c9f018c2c824189720c82fec8254",
+    "url": "/handmade-audio2/static/media/naim_audio.7c13c9f0.png"
+  },
+  {
+    "revision": "eff1662f17f56865b85c7f2e97ac81db",
+    "url": "/handmade-audio2/static/media/naim_logo.eff1662f.jpeg"
+  },
+  {
+    "revision": "ad7637200ac34a1df53da7679556026d",
+    "url": "/handmade-audio2/static/media/naim_mobile.ad763720.png"
+  },
+  {
+    "revision": "88aa51c238259b9ad06b0be9c2d207e5",
+    "url": "/handmade-audio2/static/media/statement_header.88aa51c2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93911d85bc0cb333c42dc75105f3bafd.js"
+  "/handmade-audio2/precache-manifest.1f834262f147f4052cd850d6e564afbc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/handmade-audio2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
