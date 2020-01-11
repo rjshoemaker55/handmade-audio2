7a59f689f4ff83953c4356297db117299f0e7d2f
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-01-11 07:49:31 -0500

    Updates

---
+++ precache-manifest.32b79e49104ec37a60a05595e663dd7e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cb4bf08ba11d6e5fbe116e1255f658",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2034a214f881e9bf891f",
+    "url": "/static/css/main.99acd819.chunk.css"
+  },
+  {
+    "revision": "694468a1e8f3aa69ef91",
+    "url": "/static/js/2.4d3a489e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4d3a489e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2034a214f881e9bf891f",
+    "url": "/static/js/main.6eac52f4.chunk.js"
+  },
+  {
+    "revision": "fdd32c4dd207383d0556",
+    "url": "/static/js/runtime-main.42cde561.js"
+  },
+  {
+    "revision": "acadbe795764b0529a6da5286f204788",
+    "url": "/static/media/Soundwave_logo-black.acadbe79.png"
+  },
+  {
+    "revision": "80de01a15ba85b2460ce3861401aeb6d",
+    "url": "/static/media/Soundwave_logo_white.80de01a1.png"
+  },
+  {
+    "revision": "5c7b1b15181598afc128f5097f7834c2",
+    "url": "/static/media/contact-background.5c7b1b15.jpg"
+  },
+  {
+    "revision": "4ff70a025f0e58253c9354c45abe4fab",
+    "url": "/static/media/credo_audio.4ff70a02.jpeg"
+  },
+  {
+    "revision": "e65988cbed1bad868a75ccbf8641b273",
+    "url": "/static/media/credo_audio.e65988cb.png"
+  },
+  {
+    "revision": "1f59fae8c18b3e3b74a3b825fdc7f1e1",
+    "url": "/static/media/focal_audio.1f59fae8.jpg"
+  },
+  {
+    "revision": "9efbe542e9ec874aed0ee1e17e95e920",
+    "url": "/static/media/focal_logo.9efbe542.png"
+  },
+  {
+    "revision": "c83b0154dbf6dafcf9a60de2296000f5",
+    "url": "/static/media/iso_acoustics.c83b0154.png"
+  },
+  {
+    "revision": "7c13c9f018c2c824189720c82fec8254",
+    "url": "/static/media/naim_audio.7c13c9f0.png"
+  },
+  {
+    "revision": "eff1662f17f56865b85c7f2e97ac81db",
+    "url": "/static/media/naim_logo.eff1662f.jpeg"
+  },
+  {
+    "revision": "ad7637200ac34a1df53da7679556026d",
+    "url": "/static/media/naim_mobile.ad763720.png"
+  },
+  {
+    "revision": "88aa51c238259b9ad06b0be9c2d207e5",
+    "url": "/static/media/statement_header.88aa51c2.jpg"
+  }
+]);
