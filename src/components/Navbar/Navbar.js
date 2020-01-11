9a1ff8180b61dbbfbf3f12a3708db597105c82c6
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-01-11 07:39:40 -0500

    logo added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import soundwaveLogoWhite from '../../assets/images/logos/Soundwave_logo_white.png';
+import soundwaveLogoBlack from '../../assets/images/logos/Soundwave_logo-black.png';
+
 import './styles.css';
 
 const Navbar = () => {
   return (
     <div id='nav-wrapper'>
-      <Link id='brand-item' to='/'>
-        Handmade Audio
+      <Link to='/'>
+        <img
+          className='handmade-logo'
+          id='handmade-logo-white'
+          src={soundwaveLogoWhite}
+        />
+      </Link>
+      <Link to='/'>
+        <img
+          className='handmade-logo'
+          id='handmade-logo-black'
+          src={soundwaveLogoBlack}
+        />
       </Link>
       <ul id='nav-list'>
         <li className='nav-item'>
