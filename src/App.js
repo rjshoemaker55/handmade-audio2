f2454284eeea5ecafe6ede987d2a9c77cace66b0
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-01-11 07:43:42 -0500

    fixed landing screen spacing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
